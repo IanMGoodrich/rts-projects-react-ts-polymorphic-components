a018a4b6039a27c5b8d225890aad4718684ffec3
Ian Goodrich <ian.m.goodrich.actual@gmail.com>
2025-08-12 13:10:41 -0700

    feat(input): Add input component.

---
+++ src/components/Input.tsx
@@ -0,0 +1,17 @@
+import { forwardRef ,type ComponentPropsWithoutRef} from 'react';  
+
+type InputProps = {
+  label: string;
+  id: string;
+} & ComponentPropsWithoutRef<'input'>
+
+const Input = forwardRef<HTMLInputElement, InputProps>(({ label, id, ...props }, ref) => {
+  return (
+      <p>
+        <label htmlFor={id}>{label}</label>
+        <input id={id} name={id} ref={ref} {...props} />
+      </p>
+  );
+});
+
+export default Input;
